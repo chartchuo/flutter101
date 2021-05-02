96b64225125e32d2198c97c17750b01ff34ce5ca
Chart Chongcharoen <chartchuo@gmail.com>
2021-05-02 15:22:52 +0700

    Revert "update path"
    This reverts commit 73ee1f54f23db28c558f396ce7312b9b153d591d.

--- gamesearch/android/app/src/main/kotlin/com/example/nav2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.nav2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- gamesearch/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'router.dart';
-
-void main() {
-  runApp(MaterialApp.router(
-    routeInformationParser: GameRouteInformationParser(),
-    routerDelegate: GameRouterDelegate(),
-  ));
-}

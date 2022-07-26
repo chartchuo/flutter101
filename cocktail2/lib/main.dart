20e8f86c0ecc8b52fe717caea4e3c7edd12f00f8
Chart Chongcharoen <chartchuo@gmail.com>
2022-07-26 20:00:16 +0700

    -

--- cocktail2/lib/main.dart
+++ cocktail2/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:cocktail/page/search_page.dart';
 import 'package:cocktail/repository/cocktail_repository.dart';
 import 'package:cocktail/repository/provider.dart';
 import 'package:cocktail/repository/the_cocktail_db_provider.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -18,17 +19,41 @@ void main(List<String> args) {
   provider = TheCocktailDbProvider();
   runApp(BlocProvider(
     create: (context) => CocktailBloc(CocktailRepository(provider)),
-    child: MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 2, 250, 118)),
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SearchPage(),
-        '/list': (context) => const ListPage(),
-        '/detail': (context) => const DetailPage(),
+    child: DynamicColorBuilder(
+      builder: (lightDynamic, dark) {
+        Color brandColor = const Color.fromARGB(255, 2, 250, 118);
+
+        ColorScheme lightColorScheme;
+        ColorScheme darkColorScheme;
+
+        if (lightDynamic != null && dark != null) {
+          lightColorScheme = lightDynamic.harmonized()..copyWith();
+          lightColorScheme = lightColorScheme.copyWith(secondary: brandColor);
+          darkColorScheme = dark.harmonized();
+        } else {
+          lightColorScheme = ColorScheme.fromSeed(seedColor: brandColor);
+          darkColorScheme = ColorScheme.fromSeed(
+            seedColor: brandColor,
+            brightness: Brightness.dark,
+          );
+        }
+
+        return MaterialApp(
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightColorScheme,
+          ),
+          darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: darkColorScheme,
+          ),
+          initialRoute: '/',
+          routes: {
+            '/': (context) => const SearchPage(),
+            '/list': (context) => const ListPage(),
+            '/detail': (context) => const DetailPage(),
+          },
+        );
       },
     ),
   ));
